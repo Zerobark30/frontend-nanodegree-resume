429fef914f87fcfa782f9b68807f890058e881d9
Matthew Wells <matthewpwells@gmail.com>
2015-01-10 15:25:57 -0800

    Add variable bio for biographical details, switch old variables into this one

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,11 +1,21 @@
-var role = "Web Developer"
-var name = "Matthew Wells"
-var target = "%data%"
-var header_id = "#header"
-var formattedName = HTMLheaderName.replace(target,name);
-var formattedRole = HTMLheaderRole.replace(target,role);
+var target = "%data%";
+var header_id = "#header";
+var main_id = "#main";
+var bio = {
+	"name" : "Matthew Wells",
+	"role" : "Web Developer",
+	"contacts" :  {
+		"email" : "matthewpwells@gmail.com", 
+		"phone" : "(609) 731-7278"},
+	"picture_url" : "images/Marcus_serious.jpg",
+	"welcome_message" : "Do or do not. There is no try.",
+	"skillz" : ["Data Analysis","HTML","CSS","GitHub",
+		"Chihuahua wrangling","Simpsons Watching"] 
+};
+var formattedName = HTMLheaderName.replace(target,bio.name);
+var formattedRole = HTMLheaderRole.replace(target,bio.role);
 
 $(header_id).prepend(formattedRole);
 $(header_id).prepend(formattedName);
-
+$(main_id).append(bio.picture_url);
 
