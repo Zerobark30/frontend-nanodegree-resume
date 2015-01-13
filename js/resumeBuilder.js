9d967cdb43283e26dacfd371b95fadd20e799303
Matthew Wells <matthewpwells@gmail.com>
2015-01-12 19:10:18 -0800

    Create 4 objects for work, education, bio and projects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,21 +1,103 @@
-var target = "%data%";
-var header_id = "#header";
-var main_id = "#main";
+
+var work = {
+	"jobs" : [
+		{
+			"title" : "Senior Data Analyst",
+			"employer" : "Montefiore Medical Center",
+			"years" : "2012 - present",
+			"location" : "New York, NY",
+			"description" : "Developed and maintained database management and business analytics to suport the Strategic Planning department and the Montefiore strategic plan in general"
+		},
+		{
+			"title" : "Supply Chain Analyst",
+			"employer" : "Greater New York Hospital Association",
+			"years" : "2011 - 2012",
+			"location" : "New York, NY",
+			"description" : "Created and presented analytics related to supply chain and contract optimization for hospitals in the Greater New York area"
+		},
+		{
+			"title" : "Community Health Educator",
+			"employer" : "United States Peace Corps",
+			"years" : "2008 - 2010",
+			"location" : "Turkmenistan",
+			"description" : "Taught general health education, english and teaching methodologies, in Turkmen and Uzbek to Turkmen nationals"
+		}]
+};
+
+var projects = {
+	"projects" : [
+		{
+			"title" : "ZeroBark30 Productions website",
+			"dates" : "December 2014 - present",
+			"description" : "Developing website to curate work relating to different interests"
+		}
+	]
+};
+	
 var bio = {
 	"name" : "Matthew Wells",
 	"role" : "Web Developer",
 	"contacts" :  {
 		"email" : "matthewpwells@gmail.com", 
-		"phone" : "(609) 731-7278"},
+		"phone" : "(609) 731-7278",
+		"github" : "ZeroBark30"},
 	"picture_url" : "images/Marcus_serious.jpg",
 	"welcome_message" : "Do or do not. There is no try.",
 	"skillz" : ["Data Analysis","HTML","CSS","GitHub",
 		"Chihuahua wrangling","Simpsons Watching"] 
 };
-var formattedName = HTMLheaderName.replace(target,bio.name);
-var formattedRole = HTMLheaderRole.replace(target,bio.role);
 
-$(header_id).prepend(formattedRole);
-$(header_id).prepend(formattedName);
-$(main_id).append(bio.picture_url);
-
+var education = {
+    "schools" : [
+        {
+            "school": "Quinnipaic University",
+            "graduationYear": "N/A",
+            "city": "Hamden",
+            "major": "Sociology"
+        },
+        {
+            "school": "University of North Carolina",
+            "graduationYear": "2007",
+            "city": "Chapel Hill",
+            "major": "Sociology"
+        }
+    ],
+	"onlineCourses" : [
+		{
+		"title" : "Calculus 1",
+		"school" : "Berkeley Extension",
+		"dates" : 2015,
+		"url" : "http://extension.berkeley.edu/search/publicCourseSearchDetails.do?method=load&courseId=41487"
+		},
+		{
+		"title" : "JavaScript Syntax",
+		"school" : "Udacity",
+		"dates" : 2015,
+		"url" : "http://www.udacity.com/course/ud804"
+		},
+		{
+		"title" : "How to use Git and GitHub",
+		"school" : "Udacity",
+		"dates" : 2015,
+		"url" : "http://www.udacity.com/course/ud775"
+		},
+		{
+		"title" : "Intro to HTML and CSS",
+		"school" : "Udacity",
+		"dates" : 2014,
+		"url" : "https://www.udacity.com/course/ud304"
+		},
+		{
+		"title" : "Intro to Computer Science",
+		"school" : "Udacity",
+		"dates" : 2014,
+		"url" : "https://www.udacity.com/course/cs101"
+		},
+		{
+		"title" : "Intro to systematic program design",
+		"school" : "Coursera",
+		"dates" : 2013,
+		"url" : "https://www.coursera.org/course/programdesign"
+		}
+	]
+};
